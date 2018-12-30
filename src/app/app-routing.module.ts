db1ea24f36c23626a7967df989ef444e1e158915
elroychibex@gmail.com <elroychibex@gmail.com>
2018-12-30 15:29:30 +0100

    Dashboard Done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,40 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HomeComponent } from './home/home.component';
+import { PaymentsComponent } from './payments/payments.component';
+import { SettingsComponent } from './settings/settings.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+      path: '',
+      component: LoginComponent
+    },
+    {
+      path: 'admin',
+      component: DashboardComponent,
+      children: [
+        {
+          path: '',
+          redirectTo: 'home',
+          pathMatch: 'full'
+        },
+        {
+          path: 'home',
+          component: HomeComponent,
+        },
+        {
+          path: 'payments',
+          component: PaymentsComponent,
+        },
+        {
+          path: 'settings',
+          component: SettingsComponent,
+        },        
+      ]
+    }, 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,34 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
+import { PaymentsComponent } from './payments/payments.component';
+import { SettingsComponent } from './settings/settings.component';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
+import { MatFormFieldModule, MatInputModule, MatPaginatorModule, MatTableModule, MatSortModule, MatTabsModule } from '@angular/material';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DashboardComponent,
+    HomeComponent,
+    FooterComponent,
+    PaymentsComponent,
+    SettingsComponent
   ],
   imports: [
     BrowserModule,
+    NoopAnimationsModule,
+    MatFormFieldModule, 
+    MatInputModule, 
+    MatPaginatorModule, 
+    MatTableModule, 
+    MatSortModule,
+    MatTabsModule,
     AppRoutingModule
   ],
   providers: [],
